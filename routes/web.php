41932a253a8ff8aa9efd356b35723b623bbae7b9
omgbwa-yasse <omgbwayasse@gmail.com>
2024-04-16 10:37:15 +0100

    test connexion

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Auth::routes();
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/setting', function () {
+    return view('setting');
+})->name('home');
