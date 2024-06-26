6ac325f14d79c97f654d879aa443a9f10ebabfdb
omgbwa-yasse <omgbwayasse@gmail.com>
2024-06-26 05:04:33 +0100

    Début repertoire

--- app/Http/Controllers/RecordController.php
+++ app/Http/Controllers/RecordController.php
@@ -6,117 +6,161 @@ use App\Models\RecordSupport;
 use App\Models\RecordStatus;
 use App\Models\Container;
 use App\Models\ContainerStatus;
-use App\Models\Classification;
+use App\Models\Activity;
+use App\Models\Accession;
 use App\Models\RecordLevel;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Monolog\Level;
 
+
 class RecordController extends Controller
 {
 
-    // Display a listing of the resource.
     public function index()
     {
         $records = Record::all();
+
         return view('records.index', compact('records'));
     }
 
-    // Show the form for creating a new resource.
+
     public function create()
     {
         $statuses = RecordStatus::all();
         $supports = RecordSupport::all();
-        $levels = RecordLevel::all();
-        $classifications = Classification::all();
+        $activities = Activity::all();
+        $parents = Record::all();
         $containers = Container::all();
+        $accessions = Accession::all();
         $users = User::all();
-        return view('records.create', compact('statuses', 'supports', 'classifications', 'containers', 'users', 'levels'));
+        $levels = RecordLevel::all();
+        $records = Record::all();
+        return view('records.create', compact('records','levels','statuses', 'supports', 'activities', 'parents', 'containers', 'accessions', 'users'));
     }
 
-    // Store a newly created resource in storage.
+
     public function store(Request $request)
     {
-        $request->validate([
-            'reference' => 'required',
-            'name' => 'required',
-            'date_format' => 'required',
-            'date_start' => 'required',
-            'date_end' => 'required',
-            'date_exact' => 'required',
-            'description' => 'required',
-            'level_id' => 'required',
-            'status_id' => 'required',
-            'support_id' => 'required',
-            'classification_id' => 'required',
-            'parent_id' => 'nullable',
-            'container_id' => 'required',
-            'transfer_id' => 'nullable',
-            'user_id' => 'required',
+        $validatedData = $request->validate([
+            'code' => 'required|string|max:10',
+            'name' => 'required|string',
+            'date_format' => 'required|string|max:1',
+            'date_start' => 'nullable|string|max:10',
+            'date_end' => 'nullable|string|max:10',
+            'date_exact' => 'nullable|date',
+            'level_id' => 'required|integer|exists:record_levels,id',
+            'width' => 'nullable|numeric|between:0,99999999.99',
+            'width_description' => 'nullable|string|max:100',
+            'biographical_history' => 'nullable|string',
+            'archival_history' => 'nullable|string',
+            'acquisition_source' => 'nullable|string',
+            'content' => 'nullable|string',
+            'appraisal' => 'nullable|string',
+            'accrual' => 'nullable|string',
+            'arrangement' => 'nullable|string',
+            'access_conditions' => 'nullable|string|max:50',
+            'reproduction_conditions' => 'nullable|string|max:50',
+            'language_material' => 'nullable|string|max:50',
+            'characteristic' => 'nullable|string|max:100',
+            'finding_aids' => 'nullable|string|max:100',
+            'location_original' => 'nullable|string|max:100',
+            'location_copy' => 'nullable|string|max:100',
+            'related_unit' => 'nullable|string|max:100',
+            'publication_note' => 'nullable|string',
+            'note' => 'nullable|string',
+            'archivist_note' => 'nullable|string',
+            'rule_convention' => 'nullable|string|max:100',
+            'created_at' => 'nullable|date',
+            'updated_at' => 'nullable|date',
+            'status_id' => 'required|integer|exists:record_status,id',
+            'support_id' => 'required|integer|exists:record_supports,id',
+            'activity_id' => 'required|integer|exists:activities,id',
+            'parent_id' => 'nullable|integer|exists:records,id',
+            'container_id' => 'nullable|integer|exists:containers,id',
+            'accession_id' => 'nullable|integer|exists:accessions,id',
+            'user_id' => 'required|integer|exists:users,id',
         ]);
 
-        $record = new Record();
-        $record->fill($request->all());
-        $record->user_id = Auth::id();
-        $record->save();
+        Record::create($validatedData);
 
-        return redirect()->route('records.index')
-                        ->with('success','Record created successfully.');
+        return redirect()->route('records.index')->with('success', 'Record created successfully.');
     }
 
-    // Display the specified resource.
+
     public function show(Record $record)
     {
-        return view('records.show',compact('record'));
+        return view('records.show', compact('record'));
     }
 
-    // Show the form for editing the specified resource.
+
     public function edit(Record $record)
     {
         $statuses = RecordStatus::all();
         $supports = RecordSupport::all();
-        $classifications = Classification::all();
+        $activities = Activity::all();
+        $parents = Record::all();
         $containers = Container::all();
+        $accessions = Accession::all();
         $users = User::all();
-        return view('records.edit',compact('record', 'statuses', 'supports', 'classifications', 'containers', 'users'));
+        $levels = RecordLevel::all();
+        return view('records.edit', compact('levels','record', 'statuses', 'supports', 'activities', 'parents', 'containers', 'accessions', 'users'));
     }
 
-    // Update the specified resource in storage.
+
     public function update(Request $request, Record $record)
     {
-        $request->validate([
-            'reference' => 'required',
-            'name' => 'required',
-            'date_format' => 'required',
-            'date_start' => 'required',
-            'date_end' => 'required',
-            'date_exact' => 'required',
-            'description' => 'required',
-            'level_id' => 'required',
-            'status_id' => 'required',
-            'support_id' => 'required',
-            'classification_id' => 'required',
-            'parent_id' => 'nullable',
-            'container_id' => 'required',
-            'transfer_id' => 'nullable',
-            'user_id' => 'required',
+        $validatedData = $request->validate([
+            'code' => 'required|string|max:10',
+            'name' => 'required|string',
+            'date_format' => 'required|string|max:1',
+            'date_start' => 'nullable|string|max:10',
+            'date_end' => 'nullable|string|max:10',
+            'date_exact' => 'nullable|date',
+            'level_id' => 'required|integer|exists:record_levels,id',
+            'width' => 'nullable|numeric|between:0,99999999.99',
+            'width_description' => 'nullable|string|max:100',
+            'biographical_history' => 'nullable|string',
+            'archival_history' => 'nullable|string',
+            'acquisition_source' => 'nullable|string',
+            'content' => 'nullable|string',
+            'appraisal' => 'nullable|string',
+            'accrual' => 'nullable|string',
+            'arrangement' => 'nullable|string',
+            'access_conditions' => 'nullable|string|max:50',
+            'reproduction_conditions' => 'nullable|string|max:50',
+            'language_material' => 'nullable|string|max:50',
+            'characteristic' => 'nullable|string|max:100',
+            'finding_aids' => 'nullable|string|max:100',
+            'location_original' => 'nullable|string|max:100',
+            'location_copy' => 'nullable|string|max:100',
+            'related_unit' => 'nullable|string|max:100',
+            'publication_note' => 'nullable|string',
+            'note' => 'nullable|string',
+            'archivist_note' => 'nullable|string',
+            'rule_convention' => 'nullable|string|max:100',
+            'created_at' => 'nullable|date',
+            'updated_at' => 'nullable|date',
+            'status_id' => 'required|integer|exists:record_status,id',
+            'support_id' => 'required|integer|exists:record_supports,id',
+            'activity_id' => 'required|integer|exists:activities,id',
+            'parent_id' => 'nullable|integer|exists:records,id',
+            'container_id' => 'nullable|integer|exists:containers,id',
+            'accession_id' => 'nullable|integer|exists:accessions,id',
+            'user_id' => 'required|integer|exists:users,id',
         ]);
 
-        $record->update($request->all());
+        $record->update($validatedData);
 
-        return redirect()->route('records.index')
-                        ->with('success','Record updated successfully');
+        return redirect()->route('records.index')->with('success', 'Record updated successfully.');
     }
 
-    // Remove the specified resource from storage.
+
     public function destroy(Record $record)
     {
         $record->delete();
 
-        return redirect()->route('records.index')
-                        ->with('success','Record deleted successfully');
+        return redirect()->route('records.index')->with('success', 'Record deleted successfully.');
     }
 }
-
-
