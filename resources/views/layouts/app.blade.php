610d9ca4fd3d218acd77b9841a936ca7d54697b9
Diandreas <127276266+Diandreas@users.noreply.github.com>
2024-08-31 01:48:54 +0100

    report modules

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
     <!-- CSS de Bootstrap -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <!-- Votre CSS personnalisé -->
 
     <!-- Votre JS jquery -->
@@ -17,9 +17,12 @@
 
     <title>{{ config('app.name', 'Shelve') }}</title>
     <style>
-        input,textarea,select {
+        input,
+        textarea,
+        select {
             box-shadow: inset 0 4px 8px rgba(0, 0, 0, 0.1);
         }
+
         /* Style pour le conteneur principal */
         #container {
             margin-top: 1.5rem;
@@ -30,7 +33,8 @@
         }
 
         /* Style pour l'en-tête */
-        #container h1, #container h2 {
+        #container h1,
+        #container h2 {
             background-color: #0178d4;
             color: #fff;
             padding: 1rem;
@@ -49,7 +53,8 @@
             border: 1px solid #dee2e6;
         }
 
-        #container .table th, #container .table td {
+        #container .table th,
+        #container .table td {
             border: 1px solid #dee2e6;
         }
 
@@ -60,7 +65,6 @@
         #container .table tbody tr:hover {
             background-color: #f1f1f1;
         }
-
     </style>
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
@@ -71,183 +75,201 @@
 </head>
 
 <body class="bg-light">
-<div id="app">
-    <nav class="navbar navbar-expand-md navbar-dark bg-primary shadow-sm">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="{{ url('/') }}">
-                {{ config('app.name', 'Shelve') }}
-            </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+    <div id="app">
+        <nav class="navbar navbar-expand-md navbar-dark bg-primary shadow-sm">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    {{ config('app.name', 'Shelve') }}
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <!-- Left Side Of Navbar -->
-                <form class="d-flex w-75" action="{{ route('records.search') }}">
-                    <input type="hidden" name="advanced" value="false">
-                    <input class="form-control mr-2 w-75" name="query" type="search" placeholder="Rechercher" aria-label="Search">
-                    <select class="form-select w-25" name="search_type">
-                        <option value="">Par tout</option>
-                        <option value="mail">Courriel</option>
-                        <option value="record">Archives</option>
-                        <option value="transferring_record">Archives versées</option>
-                        <option value="transferring">Versement</option>
-                    </select>
-                    <button class="btn btn-outline-light" type="submit"><i class="bi bi-search"></i></button>
-                </form>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <form class="d-flex w-75" action="{{ route('records.search') }}">
+                        <input type="hidden" name="advanced" value="false">
+                        <input class="form-control mr-2 w-75" name="query" type="search" placeholder="Rechercher"
+                            aria-label="Search">
+                        <select class="form-select w-25" name="search_type">
+                            <option value="">Par tout</option>
+                            <option value="mail">Courriel</option>
+                            <option value="record">Archives</option>
+                            <option value="transferring_record">Archives versées</option>
+                            <option value="transferring">Versement</option>
+                        </select>
+                        <button class="btn btn-outline-light" type="submit"><i class="bi bi-search"></i></button>
+                    </form>
 
-                <div class="d-grid gap-2">
+                    <div class="d-grid gap-2">
 
-                        <button type="button" name=""  id="" class="btn btn-primary" >
+                        <button type="button" name="" id="" class="btn btn-primary">
                             @php
 
                             @endphp
                         </button>
 
-                </div>
+                    </div>
 
 
-                <!-- Right Side Of Navbar -->
-                <ul class="navbar-nav ms-auto">
-                    <!-- Authentication Links -->
-                    @guest
-                        @if (Route::has('login'))
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                        @endif
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ms-auto">
+                        <!-- Authentication Links -->
+                        @guest
+                            @if (Route::has('login'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                </li>
+                            @endif
 
-                        @if (Route::has('register'))
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                            </li>
-                        @endif
-                    @else
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->name }}
-                            </a>
-
-                            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                            @endif
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                    data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }}
                                 </a>
 
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                    @csrf
-                                </form>
+                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                        onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm mt-2">
+            <div class="container-fluid">
+                <div class="navbar-nav w-100 justify-content-between">
+                    <a class="nav-link @if (Request::segment(1) == 'mails') active fw-bold text-primary @endif"
+                        href="{{ route('mails.index') }}">
+                        <i class="bi bi-envelope"></i> Courrier
+                    </a>
+                    <a class="nav-link @if (Request::segment(1) == 'repositories') active fw-bold text-primary @endif"
+                        href="{{ route('records.index') }}">
+                        <i class="bi bi-folder"></i> Repertoire
+                    </a>
+                    <a class="nav-link @if (Request::segment(1) == 'communications') active fw-bold text-primary @endif"
+                        href="{{ route('transactions.index') }}">
+                        <i class="bi bi-chat-dots"></i> Demande
+                    </a>
+                    <a class="nav-link @if (Request::segment(1) == 'transferrings') active fw-bold text-primary @endif"
+                        href="{{ route('slips.index') }}">
+                        <i class="bi bi-arrow-left-right"></i> Transfert
+                    </a>
+                    <a class="nav-link @if (Request::segment(1) == 'report') active fw-bold text-primary @endif"
+                        href="{{ route('report.dashboard') }}">
+                        <i class="bi bi-clipboard-check"></i> Rapports
+                    </a>
+                    <a class="nav-link @if (Request::segment(1) == 'deposits') active fw-bold text-primary @endif"
+                        href="{{ route('buildings.index') }}">
+                        <i class="bi bi-building"></i> Dépôt
+                    </a>
+                    <a class="nav-link @if (Request::segment(1) == 'tools') active fw-bold text-primary @endif"
+                        href="{{ route('activities.index') }}">
+                        <i class="bi bi-tools"></i> Outil
+                    </a>
+
+                    <a class="nav-link @if (Request::segment(1) == 'dollies') active fw-bold text-primary @endif"
+                        href="{{ route('dolly.index') }}">
+                        <i class="bi bi-cart3"></i> Chariots
+                    </a>
+
+                    <a class="nav-link @if (Request::segment(1) == 'settings') active fw-bold text-primary @endif"
+                        href="{{ route('mail-typology.index') }}">
+                        <i class="bi bi-gear"></i> Paramètre
+                    </a>
+
+                </div>
+            </div>
+        </nav>
+
+        <main class="py-4">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-3">
+                        <div class="card">
+                            <div class="card-body">
+                                @switch(Request::segment(1))
+                                    @case('mails')
+                                        @include('submenu.mails')
+                                    @break
+
+                                    @case('repositories')
+                                        @include('submenu.repositories')
+                                    @break
+
+                                    @case('communications')
+                                        @include('submenu.communications')
+                                    @break
+
+                                    @case('accessions')
+                                        @include('submenu.accessions')
+                                    @break
+
+                                    @case('monitorings')
+                                        @include('submenu.monitorings')
+                                    @break
+
+                                    @case('settings')
+                                        @include('submenu.settings')
+                                    @break
+
+                                    @case('deposits')
+                                        @include('submenu.deposits')
+                                    @break
+
+                                    @case('tools')
+                                        @include('submenu.tools')
+                                    @break
+
+                                    @case('transferrings')
+                                        @include('submenu.transferrings')
+                                    @break
+
+                                    @case('dollies')
+                                        @include('submenu.dollies')
+                                    @break
+
+                                    @case('report')
+                                        @include('submenu.report')
+                                    @break
+
+                                    @default
+                                        @include('submenu.mails')
+                                @endswitch
                             </div>
-                        </li>
-                    @endguest
-                </ul>
-            </div>
-        </div>
-    </nav>
-
-    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm mt-2">
-        <div class="container-fluid">
-            <div class="navbar-nav w-100 justify-content-between">
-                <a class="nav-link @if(Request::segment(1) == 'mails') active fw-bold text-primary @endif" href="{{ route('mails.index') }}">
-                    <i class="bi bi-envelope"></i> Courrier
-                </a>
-                <a class="nav-link @if(Request::segment(1) == 'repositories') active fw-bold text-primary @endif" href="{{ route('records.index') }}">
-                    <i class="bi bi-folder"></i> Repertoire
-                </a>
-                <a class="nav-link @if(Request::segment(1) == 'communications') active fw-bold text-primary @endif" href="{{ route('transactions.index') }}">
-                    <i class="bi bi-chat-dots"></i> Demande
-                </a>
-                <a class="nav-link @if(Request::segment(1) == 'transferrings') active fw-bold text-primary @endif" href="{{ route('slips.index') }}">
-                    <i class="bi bi-arrow-left-right"></i> Transfert
-                </a>
-                <a class="nav-link @if(Request::segment(1) == 'monitorings') active fw-bold text-primary @endif" href="#">
-                    <i class="bi bi-clipboard-check"></i> Rapports
-                </a>
-                <a class="nav-link @if(Request::segment(1) == 'deposits') active fw-bold text-primary @endif" href="{{ route('buildings.index') }}">
-                    <i class="bi bi-building"></i> Dépôt
-                </a>
-                <a class="nav-link @if(Request::segment(1) == 'tools') active fw-bold text-primary @endif" href="{{ route('activities.index') }}">
-                    <i class="bi bi-tools"></i> Outil
-                </a>
-
-                <a class="nav-link @if(Request::segment(1) == 'dollies') active fw-bold text-primary @endif" href="{{ route('dolly.index') }}">
-                    <i class="bi bi-cart3"></i> Chariots
-                </a>
-
-                <a class="nav-link @if(Request::segment(1) == 'settings') active fw-bold text-primary @endif" href="{{ route('mail-typology.index') }}">
-                    <i class="bi bi-gear"></i> Paramètre
-                </a>
-
-            </div>
-        </div>
-    </nav>
-
-    <main class="py-4">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-md-3">
-                    <div class="card">
-                        <div class="card-body">
-                            @switch(Request::segment(1))
-                                @case('mails')
-                                    @include('submenu.mails')
-                                    @break
-
-                                @case('repositories')
-                                    @include('submenu.repositories')
-                                    @break
-
-                                @case('communications')
-                                    @include('submenu.communications')
-                                    @break
-
-                                @case('accessions')
-                                    @include('submenu.accessions')
-                                    @break
-
-                                @case('monitorings')
-                                    @include('submenu.monitorings')
-                                    @break
-
-                                @case('settings')
-                                    @include('submenu.settings')
-                                    @break
-
-                                @case('deposits')
-                                    @include('submenu.deposits')
-                                    @break
-
-                                @case('tools')
-                                    @include('submenu.tools')
-                                    @break
-
-                                @case('transferrings')
-                                    @include('submenu.transferrings')
-                                    @break
-
-                                @case('dollies')
-                                    @include('submenu.dollies')
-                                    @break
-
-                                @default
-                                    @include('submenu.mails')
-                            @endswitch
+                        </div>
+                    </div>
+                    <div class="col-md-9">
+                        <div id="container" class="card p-4">
+                            @yield('content')
                         </div>
                     </div>
                 </div>
-                <div class="col-md-9">
-                    <div id="container" class="card p-4">
-                        @yield('content')
-                    </div>
-                </div>
             </div>
-        </div>
-    </main>
-</div>
+        </main>
+    </div>
 
-<!-- Scripts de Bootstrap -->
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-<script src="{{ asset('js/jquery-3.7.1.min.js') }}"></script>
+    <!-- Scripts de Bootstrap -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="{{ asset('js/jquery-3.7.1.min.js') }}"></script>
 </body>
 
 </html>
