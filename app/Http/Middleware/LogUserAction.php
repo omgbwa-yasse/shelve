c7b88adc6893c45458e2534e6aca291aeeb081da
omgbwa-yasse <omgbwayasse@gmail.com>
2024-10-10 19:39:19 +0100

    correction conflit

--- app/Http/Middleware/LogUserAction.php
+++ app/Http/Middleware/LogUserAction.php
@@ -14,7 +14,7 @@ use Symfony\Component\HttpFoundation\Response;
 
 class LogUserAction
 {
-<<<<<<< HEAD
+
 
     public function handle(Request $request, Closure $next)
     {
@@ -25,24 +25,6 @@ class LogUserAction
                 'user_id' => auth()->id(),
                 'action' => $request->route()->getName(),
                 'description' => auth()->user()->name .' - action performed: ' . $request->method() . ' ' . $request->path(),
-=======
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     */
-    public function handle(Request $request, Closure $next)
-    {
-        // On exécute la requête
-        $response = $next($request);
-
-        // Enregistre un log uniquement si l'utilisateur est authentifié
-        if (auth()->check()) {
-            Log::create([
-                'user_id' => auth()->id(),
-                'action' => $request->route()->getName(), // Enregistre le nom de la route comme action
-                'description' => 'Action performed: ' . $request->method() . ' ' . $request->path(),
->>>>>>> 8859924a56007ef392f95f52efcc9239e4de3630
                 'ip_address' => $request->ip(),
                 'user_agent' => $request->userAgent(),
             ]);
