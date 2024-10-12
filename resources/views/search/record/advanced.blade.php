65119ed0528f5ddfe59833b20a9199dc432e607c
omgbwa-yasse <omgbwayasse@gmail.com>
2024-10-13 00:33:05 +0100

    Ajout des données

--- resources/views/search/record/advanced.blade.php
+++ resources/views/search/record/advanced.blade.php
@@ -8,28 +8,28 @@
             <h5>Champs disponibles</h5>
             <ul class="list-group" id="fields-list">
                 <h6 class="mt-3 mb-2">Description</h6>
-                <li class="list-group-item" data-field="code" name-field="Code">Code</li>
-                <li class="list-group-item" data-field="name" name-field="Intitulé">Nom</li>
-                <li class="list-group-item" data-field="author" name-field="Producteur">Auteur</li>
-                <li class="list-group-item" data-field="content" name-field="Contenu">Contenu</li>
-                <li class="list-group-item" data-field="date_start" name-field="Date début">Date de début</li>
-                <li class="list-group-item" data-field="date_end" name-field="Date fin">Date de fin</li>
-                <li class="list-group-item" data-field="date_exact" name-field="Date exacte">Date exacte</li>
-                <li class="list-group-item" data-field="status" name-field="Statut">Statut</li>
-                <li class="list-group-item" data-field="date_creation" name-field="Date création">Date de création</li>
+                <li class="list-group-item" data-field="code" data-name-field="Code">Code</li>
+                <li class="list-group-item" data-field="name" data-name-field="Intitulé">Nom</li>
+                <li class="list-group-item" data-field="author" data-name-field="Producteur">Auteur</li>
+                <li class="list-group-item" data-field="content" data-name-field="Contenu">Contenu</li>
+                <li class="list-group-item" data-field="date_start" data-name-field="Date début">Date de début</li>
+                <li class="list-group-item" data-field="date_end" data-name-field="Date fin">Date de fin</li>
+                <li class="list-group-item" data-field="date_exact" data-name-field="Date exacte">Date exacte</li>
+                <li class="list-group-item" data-field="status" data-name-field="Statut">Statut</li>
+                <li class="list-group-item" data-field="date_creation" data-name-field="Date création">Date de création</li>
 
                 <h6 class="mt-4 mb-2">Cycle de vie</h6>
-                <li class="list-group-item" data-field="dua" name-field="Délai communicabilité">Durée de communicabilité</li>
-                <li class="list-group-item" data-field="dul" name-field="Délai légal">Durée légale</li>
+                <li class="list-group-item" data-field="dua" data-name-field="Délai communicabilité">Durée de communicabilité</li>
+                <li class="list-group-item" data-field="dul" data-name-field="Délai légal">Durée légale</li>
 
                 <h6 class="mt-4 mb-2">Localisation</h6>
-                <li class="list-group-item" data-field="container" name-field="Boite/chrono">Boite d’archives</li>
-                <li class="list-group-item" data-field="shelf" name-field="Etagère">Étagère</li>
-                <li class="list-group-item" data-field="room" name-field="Dépôt">Dépôt</li>
+                <li class="list-group-item" data-field="container" data-name-field="Boite/chrono">Boite d'archives</li>
+                <li class="list-group-item" data-field="shelf" data-name-field="Etagère">Étagère</li>
+                <li class="list-group-item" data-field="room" data-name-field="Dépôt">Dépôt</li>
 
                 <h6 class="mt-4 mb-2">Indexation</h6>
-                <li class="list-group-item" data-field="term" name-field="Terme (thésaurus)">Terme</li>
-                <li class="list-group-item" data-field="activity" name-field="Activité">Activité</li>
+                <li class="list-group-item" data-field="term" data-name-field="Terme (thésaurus)">Terme</li>
+                <li class="list-group-item" data-field="activity" data-name-field="Activité">Activité</li>
             </ul>
         </div>
 
@@ -72,21 +72,20 @@
         fieldsList.addEventListener('click', function (e) {
             if (e.target && e.target.nodeName === 'LI') {
                 const fieldName = e.target.getAttribute('data-field');
-                const name = e.target.getAttribute('name-field');
+                const name = e.target.getAttribute('data-name-field');
                 addSearchCriteria(fieldName, name);
             }
         });
 
-        function addSearchCriteria(field, Name) {
-            const criteriaClone = searchCriteriaTemplate.content.cloneNode(true);
+        function addSearchCriteria(field, name) {
+            const criteriaClone = document.importNode(searchCriteriaTemplate.content, true);
             const fieldNameInput = criteriaClone.querySelector('.field-name');
             const fieldLabel = criteriaClone.querySelector('.field-label');
             const operatorSelect = criteriaClone.querySelector('.field-operator');
+            const fieldValueInput = criteriaClone.querySelector('.field-value');
 
             fieldNameInput.value = field;
-
-
-            fieldLabel.textContent = Name.charAt(0).toUpperCase() + Name.slice(1);
+            fieldLabel.textContent = name.charAt(0).toUpperCase() + name.slice(1);
 
             // Définir les options de tri en fonction du champ
             let operators = [];
@@ -103,19 +102,17 @@
                 case 'dua':
                 case 'dul':
                     operators = ['=', '>', '<'];
+                    fieldValueInput.type = 'date';
                     break;
                 case 'container':
                 case 'shelf':
                 case 'room':
                 case 'author':
                 case 'activity':
-                case 'organisation':
-                case 'sort':
                 case 'status':
                 case 'term':
-                    operators = ['avec','sauf'];
+                    operators = ['avec', 'sauf'];
                     break;
-
             }
 
             // Ajouter les options au select
@@ -126,21 +123,22 @@
                 operatorSelect.appendChild(option);
             });
 
-
             if (['container', 'shelf', 'room', 'author', 'activity', 'term', 'status'].includes(field)) {
-            const selectElement = document.createElement('select');
-            selectElement.classList.add('form-select', 'me-2', 'field-value');
-            selectElement.name = 'value[]';
+                const selectElement = document.createElement('select');
+                selectElement.classList.add('form-select', 'me-2', 'field-value');
+                selectElement.name = 'value[]';
 
-            data[field].forEach(item => {
-                const option = document.createElement('option');
-                option.value = item.id;
-                option.textContent = item.name || item.title;
-                selectElement.appendChild(option);
-            });
+                if (data[field]) {
+                    data[field].forEach(item => {
+                        const option = document.createElement('option');
+                        option.value = item.id;
+                        option.textContent = item.name || item.title;
+                        selectElement.appendChild(option);
+                    });
+                }
 
-            fieldValueInput.replaceWith(selectElement);
-
+                fieldValueInput.replaceWith(selectElement);
+            }
 
             searchCriteriaContainer.appendChild(criteriaClone);
         }
@@ -152,5 +150,5 @@
             }
         });
     });
-    </script>
+</script>
 @endsection
