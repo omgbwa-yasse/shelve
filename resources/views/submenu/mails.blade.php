a693a886dfa26d764d61231f700b7a7a8706c92f
Diandreas <127276266+Diandreas@users.noreply.github.com>
2024-10-13 09:18:13 +0100

    menu

--- resources/views/submenu/mails.blade.php
+++ resources/views/submenu/mails.blade.php
@@ -2,118 +2,114 @@
     <div class="row">
         <!-- Recherche -->
         <a class="nav-link active bg-primary text-white" data-toggle="collapse" href="#rechercheMenu" aria-expanded="true"
-           aria-controls="rechercheMenu" style="padding: 10px;"><i class="bi bi-search"></i> Recherche</a>
+           aria-controls="rechercheMenu" style="padding: 10px;"><i class="bi bi-search"></i> {{ __('search') }}</a>
 
         <div class="collapse show" id="rechercheMenu">
             <ul class="list-unstyled pl-3">
                 <li class="nav-item">
-                    <a class="nav-link text-dark" href="{{ route('mails.index') }}"><i class="bi bi-inbox"></i> Mes courriers</a>
+                    <a class="nav-link text-dark" href="{{ route('mails.index') }}"><i class="bi bi-inbox"></i> {{ __('my_mails') }}</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-dark" href="{{ route('mail-received.index') }}"><i class="bi bi-inbox"></i> Courriers reçus</a>
+                    <a class="nav-link text-dark" href="{{ route('mail-received.index') }}"><i class="bi bi-inbox"></i> {{ __('received_mails') }}</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-dark" href="{{ route('mail-send.index') }}"><i class="bi bi-envelope"></i> Courriers envoyés</a>
+                    <a class="nav-link text-dark" href="{{ route('mail-send.index') }}"><i class="bi bi-envelope"></i> {{ __('sent_mails') }}</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-dark" href="{{ route('mails.archived') }}"><i class="bi bi-inbox"></i> Courriers archivés</a>
+                    <a class="nav-link text-dark" href="{{ route('mails.archived') }}"><i class="bi bi-inbox"></i> {{ __('archived_mails') }}</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-dark" href="{{ route('mail-author.index') }}"><i class="bi bi-envelope"></i> Producteurs</a>
+                    <a class="nav-link text-dark" href="{{ route('mail-author.index') }}"><i class="bi bi-envelope"></i> {{ __('producers') }}</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-dark" href="{{ route('typologies.index') }}"><i class="bi bi-tags"></i> Typologies</a>
+                    <a class="nav-link text-dark" href="{{ route('typologies.index') }}"><i class="bi bi-tags"></i> {{ __('typologies') }}</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-dark" href="{{ route('mail-select-date')}}"><i class="bi bi-calendar"></i> Dates</a>
+                    <a class="nav-link text-dark" href="{{ route('mail-select-date')}}"><i class="bi bi-calendar"></i> {{ __('dates') }}</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-dark" href="{{ route('mail-container.index') }}"><i class="bi bi-archive"></i> Boîtes d'archives</a>
+                    <a class="nav-link text-dark" href="{{ route('mail-container.index') }}"><i class="bi bi-archive"></i> {{ __('archive_boxes') }}</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-dark" href="{{ route('batch.index') }}"><i class="bi bi-inbox"></i> Mes parapheurs</a>
+                    <a class="nav-link text-dark" href="{{ route('batch.index') }}"><i class="bi bi-inbox"></i> {{ __('my_paraphers') }}</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-dark" href="{{ route('batch-received.index') }}"><i class="bi bi-inbox"></i> Parapheurs reçus</a>
+                    <a class="nav-link text-dark" href="{{ route('batch-received.index') }}"><i class="bi bi-inbox"></i> {{ __('received_paraphers') }}</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-dark" href="{{ route('batch-send.index') }}"><i class="bi bi-inbox"></i> Parapheurs envoyés</a>
+                    <a class="nav-link text-dark" href="{{ route('batch-send.index') }}"><i class="bi bi-inbox"></i> {{ __('sent_paraphers') }}</a>
                 </li>
             </ul>
         </div>
 
         <a class="nav-link active bg-primary rounded-2 text-white" data-toggle="collapse" href="#enregistrementMenu"
-            aria-expanded="true" aria-controls="enregistrementMenu" style="padding: 10px;">Suivi des affaires</a>
+           aria-expanded="true" aria-controls="enregistrementMenu" style="padding: 10px;">{{ __('case_follow_up') }}</a>
 
-            <div class="collapse show" id="enregistrementMenu">
+        <div class="collapse show" id="enregistrementMenu">
             <ul class="list-unstyled pl-3">
                 <li class="nav-item">
-                    <a class="nav-link text-dark" href="{{ route('mails.feedback')}}?type=true&?deadline=available"><i class="bi bi-inbox"></i> Retour attendus</a>
+                    <a class="nav-link text-dark" href="{{ route('mails.feedback')}}?type=true&?deadline=available"><i class="bi bi-inbox"></i> {{ __('expected_returns') }}</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-dark" href="{{ route('mails.feedback')}}?type=true&deadline=exceeded"><i class="bi bi-bookmark-check"></i>Retour dépassés</a>
+                    <a class="nav-link text-dark" href="{{ route('mails.feedback')}}?type=true&deadline=exceeded"><i class="bi bi-bookmark-check"></i>{{ __('overdue_returns') }}</a>
                 </li>
-
             </ul>
         </div>
 
         <!-- Créer -->
         <a class="nav-link active bg-primary text-white" data-toggle="collapse" href="#creerMenu" aria-expanded="true"
-           aria-controls="creerMenu" style="padding: 10px;">Créer</a>
+           aria-controls="creerMenu" style="padding: 10px;">{{ __('create') }}</a>
 
         <div class="collapse show" id="creerMenu">
             <ul class="list-unstyled pl-3">
                 <li class="nav-item">
-                    <a class="nav-link text-dark" href="{{ route('mails.create') }}"><i class="bi bi-inbox"></i> Courrier</a>
+                    <a class="nav-link text-dark" href="{{ route('mails.create') }}"><i class="bi bi-inbox"></i> {{ __('mail') }}</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-dark" href="{{ route('batch.create') }}"><i class="bi bi-bookmark-check"></i> Parapheur</a>
+                    <a class="nav-link text-dark" href="{{ route('batch.create') }}"><i class="bi bi-bookmark-check"></i> {{ __('parapher') }}</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-dark" href="{{ route('mail-author.create') }}"><i class="bi bi-people"></i> Producteur</a>
+                    <a class="nav-link text-dark" href="{{ route('mail-author.create') }}"><i class="bi bi-people"></i> {{ __('producer') }}</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-dark" href="{{ route('mail-container.create') }}"><i class="bi bi-archive"></i> Boîte & Chrono</a>
+                    <a class="nav-link text-dark" href="{{ route('mail-container.create') }}"><i class="bi bi-archive"></i> {{ __('box_chrono') }}</a>
                 </li>
-
             </ul>
         </div>
 
         <!-- Courrier -->
         <a class="nav-link active bg-primary text-white" data-toggle="collapse" href="#courrierMenu" aria-expanded="true"
-           aria-controls="courrierMenu" style="padding: 10px;">Courrier</a>
+           aria-controls="courrierMenu" style="padding: 10px;">{{ __('mail') }}</a>
 
         <div class="collapse show" id="courrierMenu">
             <ul class="list-unstyled pl-3">
                 <li class="nav-item">
-                    <a class="nav-link text-dark" href="{{ route('mails.inprogress') }}"><i class="bi bi-inbox"></i> A recevoir</a>
+                    <a class="nav-link text-dark" href="{{ route('mails.inprogress') }}"><i class="bi bi-inbox"></i> {{ __('to_receive') }}</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-dark" href="{{ route('mail-received.create') }}"><i class="bi bi-inbox"></i> Recevoir</a>
+                    <a class="nav-link text-dark" href="{{ route('mail-received.create') }}"><i class="bi bi-inbox"></i> {{ __('receive') }}</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-dark" href="{{ route('mail-send.create') }}"><i class="bi bi-envelope"></i> Envoyer</a>
+                    <a class="nav-link text-dark" href="{{ route('mail-send.create') }}"><i class="bi bi-envelope"></i> {{ __('send') }}</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-dark" href="{{ route('mail-archiving.create') }}"><i class="bi bi-archive"></i> Archiver </a>
+                    <a class="nav-link text-dark" href="{{ route('mail-archiving.create') }}"><i class="bi bi-archive"></i> {{ __('archive') }} </a>
                 </li>
             </ul>
         </div>
 
         <!-- Parapheur -->
         <a class="nav-link active bg-primary text-white" data-toggle="collapse" href="#parapheurMenu" aria-expanded="true"
-           aria-controls="parapheurMenu" style="padding: 10px;">Parapheur</a>
+           aria-controls="parapheurMenu" style="padding: 10px;">{{ __('parapher') }}</a>
 
         <div class="collapse show" id="parapheurMenu">
             <ul class="list-unstyled pl-3">
-
                 <li class="nav-item">
-                    <a class="nav-link text-dark" href="{{ route('batch-send.create') }}"><i class="bi bi-inbox"></i> Envoyer</a>
+                    <a class="nav-link text-dark" href="{{ route('batch-send.create') }}"><i class="bi bi-inbox"></i> {{ __('send') }}</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-dark" href="{{ route('batch-received.create') }}"><i class="bi bi-inbox"></i> Recevoir</a>
+                    <a class="nav-link text-dark" href="{{ route('batch-received.create') }}"><i class="bi bi-inbox"></i> {{ __('receive') }}</a>
                 </li>
-
             </ul>
         </div>
     </div>
--- resources/views/submenu/repositories.blade.php
+++ resources/views/submenu/repositories.blade.php
@@ -2,103 +2,96 @@
     <div class="row">
         <!-- Recherche -->
         <a class="nav-link active bg-primary rounded-2 text-white" data-toggle="collapse" href="#rechercheMenu" aria-expanded="false"
-        style="padding: 10px;" aria-controls="rechercheMenu">
-            <i class="bi bi-search"></i> Recherche
+           style="padding: 10px;" aria-controls="rechercheMenu">
+            <i class="bi bi-search"></i> {{ __('search') }}
         </a>
         <div class="collapse show" id="rechercheMenu">
             <ul class="list-unstyled pl-3">
                 <li class="nav-item">
-                    <a class="nav-link text-dark" href="{{ route('records.index') }}"><i class="bi bi-list-check"></i> Mes archives</a>
+                    <a class="nav-link text-dark" href="{{ route('records.index') }}"><i class="bi bi-list-check"></i> {{ __('my_archives') }}</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-dark" href="{{ route('record-author.index') }}"><i class="bi bi-person"></i> Detenteurs</a>
+                    <a class="nav-link text-dark" href="{{ route('record-author.index') }}"><i class="bi bi-person"></i> {{ __('holders') }}</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-dark" href="{{ route('record-select-date')}}"><i class="bi bi-calendar"></i> Dates</a>
+                    <a class="nav-link text-dark" href="{{ route('record-select-date')}}"><i class="bi bi-calendar"></i> {{ __('dates') }}</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-dark" href="{{ route('record-select-word')}}"><i class="bi bi-key"></i> Mots-clés</a>
+                    <a class="nav-link text-dark" href="{{ route('record-select-word')}}"><i class="bi bi-key"></i> {{ __('keywords') }}</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-dark" href="{{ route('record-select-activity')}}"><i class="bi bi-briefcase"></i> Activités</a>
+                    <a class="nav-link text-dark" href="{{ route('record-select-activity')}}"><i class="bi bi-briefcase"></i> {{ __('activities') }}</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-dark" href="{{ route('record-select-building')}}"><i class="bi bi-building"></i> Locaux</a>
+                    <a class="nav-link text-dark" href="{{ route('record-select-building')}}"><i class="bi bi-building"></i> {{ __('premises') }}</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-dark" href="{{ route('record-select-last')}}"><i class="bi bi-clock-history"></i> Récents</a>
+                    <a class="nav-link text-dark" href="{{ route('record-select-last')}}"><i class="bi bi-clock-history"></i> {{ __('recent') }}</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-dark" href="{{ route('records.advanced.form')}}"><i class="bi bi-search"></i> Avancées</a>
+                    <a class="nav-link text-dark" href="{{ route('records.advanced.form')}}"><i class="bi bi-search"></i> {{ __('advanced') }}</a>
                 </li>
             </ul>
         </div>
 
         <!-- Enregistrement -->
         <a class="nav-link active bg-primary rounded-2 text-white" data-toggle="collapse" href="#enregistrementMenu"
-            aria-expanded="false" style="padding: 10px;" aria-controls="enregistrementMenu">
-            <i class="bi bi-journal-plus"></i> Enregistrement
+           aria-expanded="false" style="padding: 10px;" aria-controls="enregistrementMenu">
+            <i class="bi bi-journal-plus"></i> {{ __('registration') }}
         </a>
         <div class="collapse show" id="enregistrementMenu">
             <ul class="list-unstyled pl-3">
                 <li class="nav-item">
-                    <a class="nav-link text-dark" href="{{ route('records.create') }}"><i class="bi bi-plus-square"></i> Nouveau</a>
+                    <a class="nav-link text-dark" href="{{ route('records.create') }}"><i class="bi bi-plus-square"></i> {{ __('new') }}</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-dark" href="{{ route('record-author.create') }}"><i class="bi bi-plus-square"></i> Producteur</a>
+                    <a class="nav-link text-dark" href="{{ route('record-author.create') }}"><i class="bi bi-plus-square"></i> {{ __('producer') }}</a>
                 </li>
             </ul>
         </div>
 
         <!-- lifeCycle -->
         <a class="nav-link active bg-primary rounded-2 text-white" data-toggle="collapse" href="#lifeCycleMenu" aria-expanded="false"
-            aria-controls="lifeCycleMenu" style="padding: 10px;">
-            <i class="bi bi-cart"></i> Circle de vie
+           aria-controls="lifeCycleMenu" style="padding: 10px;">
+            <i class="bi bi-cart"></i> {{ __('life_cycle') }}
         </a>
         <div class="collapse show" id="lifeCycleMenu">
             <ul class="list-unstyled pl-3">
                 <li class="nav-item">
-                    <a class="nav-link text-dark" href="{{ route('records.tostore')}}"><i class="bi bi-folder-check"></i> A transferer</a>
+                    <a class="nav-link text-dark" href="{{ route('records.tostore')}}"><i class="bi bi-folder-check"></i> {{ __('to_transfer') }}</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-dark" href="{{ route('records.toretain')}}"><i class="bi bi-folder-check"></i> Dossiers actifs</a>
+                    <a class="nav-link text-dark" href="{{ route('records.toretain')}}"><i class="bi bi-folder-check"></i> {{ __('active_files') }}</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-dark" href="{{ route('records.totransfer')}}"><i class="bi bi-arrow-right-square"></i> A verser </a>
+                    <a class="nav-link text-dark" href="{{ route('records.totransfer')}}"><i class="bi bi-arrow-right-square"></i> {{ __('to_deposit') }}</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-dark" href="{{ route('records.toeliminate')}}"><i class="bi bi-trash"></i> A éliminer</a>
+                    <a class="nav-link text-dark" href="{{ route('records.toeliminate')}}"><i class="bi bi-trash"></i> {{ __('to_eliminate') }}</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-dark" href="{{ route('records.tokeep')}}"><i class="bi bi-archive"></i> A conserver</a>
+                    <a class="nav-link text-dark" href="{{ route('records.tokeep')}}"><i class="bi bi-archive"></i> {{ __('to_keep') }}</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-dark" href="{{ route('records.tosort')}}"><i class="bi bi-sort-down"></i> A trier</a>
+                    <a class="nav-link text-dark" href="{{ route('records.tosort')}}"><i class="bi bi-sort-down"></i> {{ __('to_sort') }}</a>
                 </li>
-
             </ul>
         </div>
 
-
-        <!-- lifeCycle -->
-        <a class="nav-link active bg-primary rounded-2 text-white" data-toggle="collapse" href="#lifeCycleMenu" aria-expanded="false"
-            aria-controls="lifeCycleMenu" style="padding: 10px;">
-            <i class="bi bi-cart"></i> Import / Export (EAD, Excel, SEDA)
+        <!-- Import / Export -->
+        <a class="nav-link active bg-primary rounded-2 text-white" data-toggle="collapse" href="#importExportMenu" aria-expanded="false"
+           aria-controls="importExportMenu" style="padding: 10px;">
+            <i class="bi bi-cart"></i> {{ __('import_export') }} (EAD, Excel, SEDA)
         </a>
-        <div class="collapse show" id="lifeCycleMenu">
+        <div class="collapse show" id="importExportMenu">
             <ul class="list-unstyled pl-3">
                 <li class="nav-item">
-                    <a class="nav-link text-dark" href="{{ route('records.import.form') }}"><i class="bi bi-folder-check"></i> Import</a>
+                    <a class="nav-link text-dark" href="{{ route('records.import.form') }}"><i class="bi bi-folder-check"></i> {{ __('import') }}</a>
                 </li>
                 <li class="nav-item">
-
-                    <a class="nav-link text-dark" href="{{ route('records.export.form') }}"><i class="bi bi-folder-check"></i> export</a>
-
-
-
+                    <a class="nav-link text-dark" href="{{ route('records.export.form') }}"><i class="bi bi-folder-check"></i> {{ __('export') }}</a>
+                </li>
             </ul>
         </div>
-
-
     </div>
 </div>
