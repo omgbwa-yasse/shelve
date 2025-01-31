9b76ca65202f94159f4c3135d99a69ca11ac0469
omgbwa-yasse <omgbwayasse@gmail.com>
2025-01-31 11:32:52 -0500

    add migrations for AI module and deposit management

---
+++ database/migrations/2025_01_31_151734_create_ai_module_table.php
@@ -0,0 +1,68 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAiModuleTable extends Migration
+{
+    public function up()
+    {
+        Schema::dropIfExists('prompts');
+        Schema::dropIfExists('ai_agents');
+        Schema::dropIfExists('ai_model_configs');
+
+
+        // Prompts
+        Schema::create('prompts', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('instruction');
+            $table->boolean('is_public')->default(false);
+            $table->boolean('is_system')->default(false);
+            $table->timestamps();
+        });
+
+
+        // Agents AI
+        Schema::create('ai_agents', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 150)->unique(true);
+            $table->text('description');
+
+            // date
+            $table->date('date_start')->nullable();
+            $table->date('date_end')->nullable();
+            $table->date('date_exact')->nullable();
+            $table->enum('date_type', ['start_only', 'exact', 'range'])->default('start_only');
+
+            // Fréquence
+            $table->enum('frequence_type', ['day', 'heure', 'min'])->default('day');
+            $table->integer('frequence_value');
+
+            // clé étrangères
+            $table->foreignId('prompt_id')->constrained('prompts');
+            $table->foreignId('user_id')->constrained('users');
+            $table->boolean('is_public')->default(false);
+            $table->timestamps();
+        });
+
+
+        Schema::create('ai_model_configs', function (Blueprint $table) {
+            $table->id();
+            $table->string('model_name');
+            $table->string('config_key');
+            $table->text('value');
+            $table->timestamps();
+        });
+
+
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('ai_model_configs');
+        Schema::dropIfExists('ai_agents');
+        Schema::dropIfExists('prompts');
+    }
+}
