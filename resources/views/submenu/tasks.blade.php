cfef8ea927c3e834a8489c58e000586928ee08f5
omgbwa-yasse <omgbwayasse@gmail.com>
2025-02-02 01:27:44 -0500

    refactor tasks view: simplify navigation and enhance project section with new links

--- resources/views/submenu/tasks.blade.php
+++ resources/views/submenu/tasks.blade.php
@@ -1,36 +1,5 @@
 <div class="container" style="background-color: #f1f1f1;">
     <div class="row">
-        <!-- Search -->
-        <a class="nav-link active bg-primary text-white" data-toggle="collapse" href="#searchMenu" aria-expanded="true" aria-controls="searchMenu" style="padding: 10px;">
-            <i class="bi bi-search"></i> {{ __('search') }}
-        </a>
-        <div class="collapse show" id="searchMenu">
-            <ul class="list-unstyled pl-3">
-                <li class="nav-item">
-                    <a class="nav-link text-dark" href="{{ route('tasks.index') }}"><i class="bi bi-inbox"></i> {{ __('tasks') }}</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-dark" href="{{ route('tasks.index') }}"><i class="bi bi-inbox"></i> Workflow </a>
-                </li>
-                <!-- Add other search links here -->
-            </ul>
-        </div>
-
-        <!-- Create -->
-        <a class="nav-link active bg-primary text-white" data-toggle="collapse" href="#createMenu" aria-expanded="true" aria-controls="createMenu" style="padding: 10px;">
-            {{ __('create') }}
-        </a>
-        <div class="collapse show" id="createMenu">
-            <ul class="list-unstyled pl-3">
-                <li class="nav-item">
-                    <a class="nav-link text-dark" href="{{ route('tasks.create') }}"><i class="bi bi-inbox"></i> {{ __('task') }}</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-dark" href="{{ route('tasks.create') }}"><i class="bi bi-inbox"></i> Workflow </a>
-                </li>
-                <!-- Add other creation links here -->
-            </ul>
-        </div>
 
         <!-- Task Tracking -->
         <a class="nav-link active bg-primary text-white" data-toggle="collapse" href="#trackingMenu" aria-expanded="true" aria-controls="trackingMenu" style="padding: 10px;">
@@ -39,16 +8,22 @@
         <div class="collapse show" id="trackingMenu">
             <ul class="list-unstyled pl-3">
                 <li class="nav-item">
-                    <a class="nav-link text-dark" href="{{ route('tasks.index') }}"><i class="bi bi-inbox"></i> {{ __('ongoing') }}</a>
+                    <a class="nav-link text-dark" href="{{ route('tasks.create') }}"><i class="bi bi-plus-circle"></i> {{ __('new task') }}</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-dark" href="{{ route('tasks.index') }}"><i class="bi bi-bookmark-check"></i> {{ __('completed') }}</a>
+                    <a class="nav-link text-dark" href="{{ route('tasks.index') }}"><i class="bi bi-collection"></i> {{ __('all task') }}</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-dark" href="{{ route('tasks.myTasks') }}"><i class="bi bi-bookmark-check"></i> {{ __('list') }}</a>
+                    <a class="nav-link text-dark" href="{{ route('tasks.index') }}"><i class="bi bi-hourglass-split"></i> {{ __('ongoing') }}</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-dark" href="{{ route('tasks.supervision') }}"><i class="bi bi-bookmark-check"></i> {{ __('supervision') }}</a>
+                    <a class="nav-link text-dark" href="{{ route('tasks.index') }}"><i class="bi bi-check2-circle"></i> {{ __('completed') }}</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-dark" href="{{ route('tasks.myTasks') }}"><i class="bi bi-person-workspace"></i> {{ __('My tasks') }}</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-dark" href="{{ route('tasks.supervision') }}"><i class="bi bi-eye"></i> {{ __('supervision') }}</a>
                 </li>
             </ul>
         </div>
@@ -61,19 +36,46 @@
         <div class="collapse show" id="trackingWorkflow">
             <ul class="list-unstyled pl-3">
                 <li class="nav-item">
-                    <a class="nav-link text-dark" href=""><i class="bi bi-inbox"></i> En cours</a>
+                    <a class="nav-link text-dark" href=""><i class="bi bi-plus-circle"></i> New</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-dark" href=""><i class="bi bi-bookmark-check"></i> Effectué </a>
+                    <a class="nav-link text-dark" href=""><i class="bi bi-hourglass-split"></i> En cours</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-dark" href=""><i class="bi bi-bookmark-check"></i> Supervisé </a>
+                    <a class="nav-link text-dark" href=""><i class="bi bi-check2-circle"></i> Effectué</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-dark" href=""><i class="bi bi-bookmark-check"></i> Liste </a>
+                    <a class="nav-link text-dark" href=""><i class="bi bi-eye"></i> Supervisé</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-dark" href=""><i class="bi bi-list-ul"></i> Liste</a>
                 </li>
             </ul>
         </div>
+
+
+
+
+        <!-- Section Projet -->
+    <a class="nav-link active bg-primary text-white" data-toggle="collapse" href="#projectSection" aria-expanded="true" aria-controls="projectSection" style="padding: 10px;">
+        Projet**
+    </a>
+    <div class="collapse show" id="projectSection">
+        <ul class="list-unstyled pl-3">
+            <li class="nav-item">
+                <a class="nav-link text-dark" href=""><i class="bi bi-clipboard-data"></i> Tableau de bord</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-dark" href=""><i class="bi bi-plus-circle"></i> Nouveau projet</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-dark" href=""><i class="bi bi-list-check"></i> Liste des projets</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-dark" href=""><i class="bi bi-archive"></i> Archives</a>
+            </li>
+        </ul>
+    </div>
 
 
     </div>
