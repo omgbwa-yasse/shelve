7b7f2208273f9eb6ee9aeca6807bbc7048b005d3
omgbwa-yasse <omgbwayasse@gmail.com>
2025-06-23 13:47:49 -0400

    fix: Correct class name casing for OrganisationActivity model and add MySQL database connection configuration

--- app/Models/OrganisationActivity.php
+++ app/Models/OrganisationActivity.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class organisationActivity extends Model
+class OrganisationActivity extends Model
 {
     use HasFactory;
 
