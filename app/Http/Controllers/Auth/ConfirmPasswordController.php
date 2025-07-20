58446e2a1bc727c57b449835bca39548722720e1
omgbwa-yasse <omgbwayasse@gmail.com>
2025-07-20 19:19:02 -0400

    refactor(auth): update redirect paths to point to /repositories/records and remove PolicyTestController

--- app/Http/Controllers/Auth/ConfirmPasswordController.php
+++ app/Http/Controllers/Auth/ConfirmPasswordController.php
@@ -25,7 +25,7 @@ class ConfirmPasswordController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/mails/file';
+    protected $redirectTo = '/repositories/records';
 
     /**
      * Create a new controller instance.
