ab72279a21d9841d99f23a978929cfdd555da021
omgbwa-yasse <omgbwayasse@gmail.com>
2025-08-17 16:21:52 -0400

    feat(records): enable PDF export option; handle format=pdf in exportButton

--- resources/views/records/index.blade.php
+++ resources/views/records/index.blade.php
@@ -27,6 +27,7 @@
                             <option value="excel">Excel</option>
                             <option value="ead">EAD</option>
                             <option value="seda">SEDA</option>
+                            <option value="pdf">PDF</option>
                         </select>
                     </div>
 
