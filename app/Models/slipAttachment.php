66124ec349c9deedb03c24963623f4f55ab1689d
omgbwa-yasse <omgbwayasse@gmail.com>
2025-08-20 17:20:23 -0400

    fix: correct class name casing for SlipAttachment model

--- app/Models/slipAttachment.php
+++ app/Models/slipAttachment.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class slipAttachment extends Model
+class SlipAttachment extends Model
 {
     use HasFactory;
 
