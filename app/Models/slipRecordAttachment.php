d7a566c78e7a0ff78b621affa04b79f4d412214d
omgbwa-yasse <omgbwayasse@gmail.com>
2025-08-20 17:12:41 -0400

    fix: correct class definition and namespace structure in SlipRecordAttachment model

--- app/Models/slipRecordAttachment.php
+++ app/Models/slipRecordAttachment.php
@@ -19,8 +19,14 @@ class SlipRecordAttachment extends Model
     {
         return $this->belongsTo(SlipRecord::class, 'slip_record_id');
     }
-
-
-
-
 }
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SlipRecordAttachment extends Model
+{
+    use HasFactory;
